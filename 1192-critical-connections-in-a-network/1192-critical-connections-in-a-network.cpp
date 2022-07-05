b7f1cf7442b0badfc88e00eb8266ced2c871b2d7
Anirudh Shivarkar <40207836+Anirudh906@users.noreply.github.com>
2022-07-05 16:42:09 +0530

    Time: 930 ms (50.48%), Space: 166.8 MB (86.61%) - LeetHub

---
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int t = 0;
+    void solve(int u, vector<bool>& vis, vector<int>& disc, vector<int>& low,     vector<int>& parent, vector<vector<int>>& g, vector<vector<int>>& res) {
+        
+        vis[u] = true;
+        disc[u] = low[u] = ++t;
+        for (int v : g[u]) {
+            if (!vis[v]) {
+                parent[v] = u;
+                solve(v, vis, disc, low, parent, g, res);
+                low[u] = min(low[u], low[v]);
+                if (low[v] > disc[u]) {
+                    res.push_back({u, v});
+                }
+
+            } else if (v != parent[u]) {
+                low[u] = min(low[u], disc[v]);
+            }
+
+        }
+    } 
+    
+    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
+    vector<bool> vis(n, false);
+        vector<int> disc(n, 0), low(n, 0), parent(n, -1);
+        vector<vector<int>> g(n), res;
+        for (auto & p : connections) {
+            g[p[0]].push_back(p[1]);
+            g[p[1]].push_back(p[0]);
+        }
+        for (int i = 0; i < n; ++i) {
+            if (vis[i]) continue;
+            solve(i, vis, disc, low, parent, g, res);
+        }
+        return res;
+    }
+};
