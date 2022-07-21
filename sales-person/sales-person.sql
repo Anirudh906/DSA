8e13b1f0399fa98e4d7c6c0d24e288bc24802c63
Anirudh Shivarkar <40207836+Anirudh906@users.noreply.github.com>
2022-07-21 15:59:22 +0530

    Time: 2133 ms, Memory: 0B - LeetHub

--- sales-person/sales-person.sql
+++ sales-person/sales-person.sql
@@ -1,7 +1,2 @@
 # Write your MySQL query statement below
-SELECT name from salesperson
-where sales_id not in 
-(
-    select sales_id from orders where com_id in 
-	(select com_id from company where name='RED')
-)
+SELECT salesperson.name FROM orders o join company c on (o.com_id = c.com_id and c.name="RED") right join salesperson on (salesperson.sales_id = o.sales_id) WHERE o.sales_id IS NULL
