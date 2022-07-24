b65d5b767605eeec6a21cd6799bb46e7473f2da9
Anirudh Shivarkar <40207836+Anirudh906@users.noreply.github.com>
2022-07-24 13:45:41 +0530

    Time: 666 ms (5.07%), Space: 131 MB (20.62%) - LeetHub

---
+++ 801-minimum-swaps-to-make-sequences-increasing/801-minimum-swaps-to-make-sequences-increasing.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int solve(vector<int> &nums1, vector<int> &nums2, vector<vector<int>> &dp, int i1, int swapped){
+       
+        if(i1 == nums1.size()){
+            return 0;
+        }
+        if(dp[i1][swapped] != -1) return dp[i1][swapped];
+        int prev1 = nums1[i1-1];
+        int prev2 = nums2[i1-1];
+        
+        
+        if(swapped){
+            swap(prev1, prev2);
+        }
+       
+        int ans = INT_MAX;
+        
+        
+        if(nums1[i1] > prev1 and nums2[i1] > prev2) {
+              ans = min(ans, solve(nums1, nums2, dp, i1+1, 0));
+        } 
+        
+        if(nums1[i1] > prev2 and nums2[i1] > prev1) {
+              ans = min(ans, 1 + solve(nums1, nums2, dp, i1+1, 1));
+        }  
+        
+        
+        return dp[i1][swapped] = ans;
+    }
+   
+    int minSwap(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+        nums1.insert(nums1.begin(), -1);
+        nums2.insert(nums2.begin(), -1);
+        vector<vector<int>> dp(n+1, vector<int>(2, -1));
+        return solve(nums1, nums2, dp, 1, 0);
+    }
+};
