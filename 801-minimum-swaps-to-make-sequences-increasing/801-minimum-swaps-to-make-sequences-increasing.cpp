97d6cce67d2d240d2abd45423cd508b4791d3f5d
Anirudh Shivarkar <40207836+Anirudh906@users.noreply.github.com>
2022-07-24 15:26:18 +0530

    Time: 554 ms (7.49%), Space: 120.5 MB (26.27%) - LeetHub

--- 801-minimum-swaps-to-make-sequences-increasing/801-minimum-swaps-to-make-sequences-increasing.cpp
+++ 801-minimum-swaps-to-make-sequences-increasing/801-minimum-swaps-to-make-sequences-increasing.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int solve(vector<int> &nums1, vector<int> &nums2, vector<vector<int>> &dp, int i1, int swapped){
-       
+        
         if(i1 == nums1.size()){
             return 0;
         }
@@ -32,6 +32,8 @@ public:
     int solve2(vector<int> &nums1, vector<int> &nums2){
         int n = nums1.size();
         vector<vector<int>> dp(n+1, vector<int>(2));
+        
+        int nxtSwap = 0, nxtNoSwap = 0, curSwap = 0, curNoSwap = 0; 
         for(int i = n-1; i >= 1; i--){
             for(int j = 1; j >= 0; j--){
                    int prev1 = nums1[i-1];
@@ -43,19 +45,21 @@ public:
                         swap(prev1, prev2);
                     }
                     if(nums1[i] > prev1 and nums2[i] > prev2) {
-                          ans = min(ans, dp[i+1][0]);
+                          ans = min(ans, nxtNoSwap);
                     } 
         
                     if(nums1[i] > prev2 and nums2[i] > prev1) {
-                          ans = min(ans, 1 + dp[i+1][1]);
+                          ans = min(ans, 1 + nxtSwap);
                     }  
         
-        
-              dp[i][j] = ans;
+               if(j) curSwap = ans;
+               else curNoSwap = ans;
         
             }
+            nxtNoSwap = curNoSwap;
+            nxtSwap = curSwap;
         }
-        return dp[1][0];
+        return min(nxtSwap, nxtNoSwap);
     }
    
     int minSwap(vector<int>& nums1, vector<int>& nums2) {
