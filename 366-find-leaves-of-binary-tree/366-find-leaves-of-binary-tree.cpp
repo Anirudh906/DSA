97ace03dd06be8df476578fb06d9cdc692bb1f08
Anirudh Shivarkar <40207836+Anirudh906@users.noreply.github.com>
2022-08-06 14:31:32 +0530

    Time: 4 ms (40.70%), Space: 8.8 MB (72.64%) - LeetHub

--- 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.cpp
+++ 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.cpp
@@ -11,17 +11,54 @@
  */
 class Solution {
 public:
-    int dfs(TreeNode *root, vector<vector<int>> &res){
-        if(!root)return 0;
-        int lvl = max(dfs(root -> left, res), dfs(root -> right, res)) + 1;
-        if(lvl > res.size())res.push_back(vector<int>());
-        res[lvl - 1].push_back(root -> val);
-        return lvl;
-    }
-    
     vector<vector<int>> findLeaves(TreeNode* root) {
         vector<vector<int>> res;
-        dfs(root, res);
+        
+        unordered_map<TreeNode*, int> inDeg;
+        unordered_map<TreeNode*, vector<TreeNode*>> mp;
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        while(!q.empty()){
+            auto t = q.front();
+            int child = 0;
+            q.pop();
+            if(t -> left) {
+                child++;
+                mp[t -> left].push_back(t);
+                q.push(t -> left);
+            } if(t -> right){
+                child++;
+                mp[t -> right].push_back(t);
+                q.push(t -> right);
+            }
+            
+           inDeg[t] = child;
+        }
+        
+        
+        queue<TreeNode *> q2;
+        
+        for(auto &x: inDeg){
+            if(x.second == 0) q2.push(x.first);
+        }
+        
+        while(!q2.empty()){
+            int s = q2.size();
+            vector<int> temp;
+         for(int i = 0; i < s; i++){
+            auto t = q2.front();
+             temp.push_back(t -> val);
+            q2.pop();
+            
+            for(auto &x: mp[t]){
+                inDeg[x]--;
+                if(inDeg[x] == 0) q2.push(x);
+            }
+          }
+            res.push_back(temp);
+        }
         return res;
     }
 };
