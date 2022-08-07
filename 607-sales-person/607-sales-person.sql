7d98f7e9fc422c8ba86055eff5465e008e2074d9
Anirudh Shivarkar <40207836+Anirudh906@users.noreply.github.com>
2022-08-07 17:08:27 +0530

    Time: 1053 ms (86.31%), Space: 0B (100.00%) - LeetHub

---
+++ 607-sales-person/607-sales-person.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+SELECT salesperson.name FROM orders o join company c on (o.com_id = c.com_id and c.name="RED") right join salesperson on (salesperson.sales_id = o.sales_id) WHERE o.sales_id IS NULL
