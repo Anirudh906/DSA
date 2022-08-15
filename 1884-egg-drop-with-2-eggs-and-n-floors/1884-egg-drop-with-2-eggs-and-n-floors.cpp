37d210be1633593c109cce5acaa21db45e3b57da
Anirudh Shivarkar <40207836+Anirudh906@users.noreply.github.com>
2022-08-15 18:58:06 +0530

    Time: 10 ms (62.81%), Space: 8.9 MB (11.09%) - LeetHub

---
+++ 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int twoEggDrop(int f) {
+        vector<vector<int>> dp(f+1, vector<int> (3));
+       
+        for(int i = 1; i <= 2; i++){
+            dp[0][i] = 0; dp[1][i] = 1;
+        }
+         for(int i = 1; i <= f; i++){
+            dp[i][1] = i; dp[i][0] = 0;
+        }
+        for(int i = 2; i <= f; i++){
+            for(int j = 2; j <= 2; j++){
+                dp[i][j] = INT_MAX;
+                 int l = 1, r = i, ans = INT_MAX, temp = 0;
+                while(l <= r){
+                    int m = l + (r - l)/2;
+                     int left = dp[m - 1][j - 1]; 
+                    int right = dp[i - m][j]; 
+                    temp = 1 + max(left, right);          
+                    if(left < right){                  
+                      l = m + 1;                      
+                    }
+                    else {    
+                       r = m - 1;
+                    }
+                    ans = min(ans, temp);               
+                 }
+                 dp[i][j] = ans;
+                
+            }
+      }
+    
+     return dp[f][2];   
+        
+    }
+};
