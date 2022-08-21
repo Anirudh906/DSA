0191ad46d965975d2996bd0d8c37a4d4b1638332
Anirudh Shivarkar <40207836+Anirudh906@users.noreply.github.com>
2022-08-21 09:25:28 +0530

    Time: 0 ms (100.00%), Space: 6 MB (37.61%) - LeetHub

--- 1611-minimum-one-bit-operations-to-make-integers-zero/1611-minimum-one-bit-operations-to-make-integers-zero.cpp
+++ 1611-minimum-one-bit-operations-to-make-integers-zero/1611-minimum-one-bit-operations-to-make-integers-zero.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
-    int minimumOneBitOperations(int n) {
-          if (n <= 1) return n;
-        int l = 32 - __builtin_clz(n);
-        int mask = (1 << (l - 1)) ^ n;
-        return (1 << l) - 1 - minimumOneBitOperations(mask);
+  int minimumOneBitOperations(int n){
+    for (int i=30; i >= 0; i--) if (n & (1 << i)) {
+        return (1 << (i+1)) - minimumOneBitOperations(n & ~(1 << i)) - 1;
     }
+    return 0;
+}
 };
