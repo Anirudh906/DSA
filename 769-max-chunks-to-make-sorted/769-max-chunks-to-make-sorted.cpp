6cd63d6b66b8e9d95352ba6da789f5a8a13f6787
Anirudh Shivarkar <40207836+Anirudh906@users.noreply.github.com>
2022-08-27 21:11:46 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (28.67%) - LeetHub

---
+++ 769-max-chunks-to-make-sorted/769-max-chunks-to-make-sorted.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+       int n = arr.size();
+        stack<int> st;
+        for(int i = 0; i < n; i++){
+            int temp = arr[i];
+            while(st.size() > 0 and arr[i] < st.top()){
+                int t = st.top();
+                temp = max(temp, t);
+                st.pop();
+            }
+            st.push(temp);
+        }
+        return (int)st.size();
+    }
+};
