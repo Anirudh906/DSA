8eb708d7f31ce89ee8a8beb415664f23acdbcdc7
Anirudh Shivarkar <40207836+Anirudh906@users.noreply.github.com>
2024-08-05 08:37:58 +0530

    Time: 75 ms (82.03%), Space: 11.4 MB (78.57%) - LeetHub

---
+++ 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    static const int mod = 1e9 + 7;
+    int rangeSum(vector<int>& nums, int n, int left, int right) {
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        for(int i = 0; i < n; i++){
+            pq.push({nums[i], i+1});
+        }
+        int ans = 0;
+        for(int i = 1; i <= right; i++){
+            auto t = pq.top();
+            pq.pop();
+            if(i >= left){
+                ans = (ans + t.first) % mod;
+            }
+            if(t.second < n){
+                t.first += nums[t.second];
+                t.second++;
+                pq.push(t);
+            }
+        }
+        return ans;
+    }
+};
